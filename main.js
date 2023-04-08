b8a425ef5d5e8314e14ddd04294397ba2c5f6d03
Sanjar2010 <sanzarkemelov432@gmail.com>
2023-04-08 15:58:43 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,58 @@
+
+
+
+//nomer 1
+
+// function Person(name, age) {
+//     this.name = name;
+//     this.age = age;
+  
+//     this.func = function() {
+//       console.log(`Меня зовут ${this.name}, мне ${this.age} лет`);
+//     }
+//   }
+//   const person = new Person("John", 25);
+
+//   const friend = {
+//     name: "Alex",
+//     age: 30
+//   };
+  
+
+//   person.func.call(friend);
+
+
+
+
+//nomer2
+// people.forEach(function(person) {
+//     console.log(person.marks.length);
+//   });
+// function sayHello(manager) {
+//     console.log(${this.name}, ваша зарплата ${this.salary}, вы получаете повышение теперь ваша зарплата ${this.salary * 2} );
+// }
+
+// const person = {
+//     name: 'Artur',
+//     salary: 5000
+// }
+
+// sayHello.call(person)
+
+//nomer 3
+// function middleAverage(num){
+//     const sum = 0
+//     const count = 0
+//     return num.forEach(element => {
+//         sum += +count
+//         return sum
+//     }); 
+// }
+// const student = {
+//     name: "Sanjar",
+//     age: 12,
+//     marks: function() {
+//         middleAverage([4,4,4,4])
+//     }
+// }
+
